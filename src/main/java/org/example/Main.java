8343b368d7950b0e6dab455a95b0450600e365ca
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 17:20:35 +0300

    modification de main pour correspondre l'affichage des resultats

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,35 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         DataRetriever dr = new DataRetriever();
+//q1
+        long totalVote = dr.countAllVotes();
+        System.out.println("totalVote=" + totalVote);
+//q2
+        List<VoteTypeCount> voteTypeCounts = dr.countVotesByType();
+        System.out.println(voteTypeCounts);
+//q3
+        List<CandidateVoteCount> candidateResults =
+                dr.countValidVotesByCandidate();
+        System.out.println(candidateResults);
+//q4
+        VoteSummary summary = dr.computeVoteSummary();
+        System.out.println(summary);
+//q5
+        double turnoutRate = dr.computeTurnoutRate();
+        long totalVoters = dr.countTotalVoters(); // petite méthode ajoutée
 
-        System.out.println("Q1 total votes = " + dr.countAllVotes());
-        System.out.println("Q2 votes by type = " + dr.countVotesByType());
-        System.out.println("Q3 valid votes by candidate = " + dr.countValidVotesByCandidate());
-        System.out.println("Q4 summary = " + dr.computeVoteSummary());
-        System.out.println("Q5 turnout rate = " + dr.computeTurnoutRate() + "%");
-        System.out.println("Q6 winner = " + dr.findWinner());
+        System.out.println("Nombre total d’électeurs : " + totalVoters);
+        System.out.println("Nombre de votes enregistrés : " + totalVote);
+        System.out.println("Donc taux de participations = "
+                + turnoutRate + "%");
+//q6
+        ElectionResult winner = dr.findWinner();
+        System.out.println(winner);
     }
 }
