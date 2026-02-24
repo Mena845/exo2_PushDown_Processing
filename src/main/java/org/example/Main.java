590eb04a18d43ca5d6e26217192a2428bf49cdd0
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 17:01:28 +0300

    modification de Main pour tester tous les methodes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        DataRetriever dr = new DataRetriever();
+
+        System.out.println("Q1 total votes = " + dr.countAllVotes());
+        System.out.println("Q2 votes by type = " + dr.countVotesByType());
+        System.out.println("Q3 valid votes by candidate = " + dr.countValidVotesByCandidate());
+        System.out.println("Q4 summary = " + dr.computeVoteSummary());
+        System.out.println("Q5 turnout rate = " + dr.computeTurnoutRate() + "%");
+        System.out.println("Q6 winner = " + dr.findWinner());
     }
 }
