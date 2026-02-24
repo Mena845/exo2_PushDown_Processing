2382139e871fd0b48b927b25ceab8a86f506825a
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 16:36:05 +0300

    creation de la classe DBConnection

---
+++ src/main/java/org/example/DBConnection.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/ceni";
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "tsilakely2220";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
