b2f5dc685557cc515bf60cff0171644ec52e8509
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 16:40:09 +0300

    ajout de la methode coutAllVotes dans DataRetriever

--- src/main/java/org/example/DataRetriever.java
+++ src/main/java/org/example/DataRetriever.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataRetriever {
+
+
+//    q1 = nombre total des votes
+    public long countAllVotes() {
+
+        String sql = "SELECT COUNT(*) AS total_votes FROM vote";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                return rs.getLong("total_votes");
+            }
+            return 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
