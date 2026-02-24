41ba489d93d36ed1c2e5dc2ad70a6d83b5b8b296
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 16:46:16 +0300

    creation de la classe VoteTypeCount

---
+++ src/main/java/org/example/VoteTypeCount.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class VoteTypeCount {
+
+    private String voteType;
+    private long count;
+
+    public VoteTypeCount(String voteType, long count) {
+        this.voteType = voteType;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return voteType + "=" + count;
+    }
+}
