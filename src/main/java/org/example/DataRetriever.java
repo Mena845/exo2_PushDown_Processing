f6a27555b8fd1432824a1ce0a389113294137676
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 16:44:13 +0300

    ajout de la methode countVotesByType dans DataRetriever

--- src/main/java/org/example/DataRetriever.java
+++ src/main/java/org/example/DataRetriever.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataRetriever {
 
@@ -21,6 +23,35 @@ public class DataRetriever {
                 return rs.getLong("total_votes");
             }
             return 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+//     q2 = vote par type
+    public List<VoteTypeCount> countVotesByType() {
+
+        String sql = """
+            SELECT vote_type, COUNT(*) AS count
+            FROM vote
+            GROUP BY vote_type
+        """;
+
+        List<VoteTypeCount> result = new ArrayList<>();
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                result.add(new VoteTypeCount(
+                        rs.getString("vote_type"),
+                        rs.getLong("count")
+                ));
+            }
+            return result;
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
