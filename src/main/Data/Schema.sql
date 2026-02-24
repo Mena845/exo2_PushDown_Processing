c69f6b5502ebd938cf739b9bf4939764bf6ce283
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 16:17:07 +0300

    creation de la table vote

--- src/main/Data/Schema.sql
+++ src/main/Data/Schema.sql
@@ -14,3 +14,13 @@ CREATE TABLE voter (
                        id SERIAL PRIMARY KEY,
                        name TEXT NOT NULL
 );
+
+
+
+-- creation de la table vote
+CREATE TABLE vote (
+                      id SERIAL PRIMARY KEY,
+                      candidate_id INT REFERENCES candidate(id),
+                      voter_id INT NOT NULL REFERENCES voter(id),
+                      vote_type vote_type NOT NULL
+);
