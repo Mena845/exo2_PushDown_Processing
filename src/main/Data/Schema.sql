e86ceed8c87cfae8f0a04820852083a641892a70
mena <tsikiniainatsilavina@gmail.com>
2026-02-24 16:14:45 +0300

    creation de la table candidate

--- src/main/Data/Schema.sql
+++ src/main/Data/Schema.sql
@@ -1,2 +1,9 @@
 -- creation du type enum
 CREATE TYPE vote_type AS ENUM ('VALID', 'BLANK', 'NULL');
+
+
+-- creation de la table candidate
+CREATE TABLE candidate (
+                           id SERIAL PRIMARY KEY,
+                           name TEXT NOT NULL
+);
